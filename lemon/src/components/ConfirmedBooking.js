487ea47963d88c1e559ef656e40b5061e26e02b1
Wirelessscript <nieuwenhuisjohan309@gmail.com>
2024-08-23 00:25:04 +0200

    Bookings added

---
+++ lemon/src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ConfirmedBooking = (props) => {
+    return (
+        <div>
+            <div>
+                <h1>Thank you!Your Booking has been <span>confirmed!</span></h1>
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmedBooking;
