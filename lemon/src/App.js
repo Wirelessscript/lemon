0dbd652f837814266ce07e37242ea353b04c29c1
Wirelessscript <nieuwenhuisjohan309@gmail.com>
2024-08-22 20:32:41 +0200

    new style

--- lemon/src/App.js
+++ lemon/src/App.js
@@ -1,22 +1,15 @@
+import React from 'react';
 import './App.css'
-import Header from './Header';
-import Footer from './Footer';
-import Main from './Main';
-import Specials from './Specials';
-import Testimonials from './Testimonials';
-import About from './About';
+import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
-  return(
-      <>
-      <Header/>
-      <Main/>
-      <Specials/>
-      <Testimonials/>
-      <About/>
-      <Footer/>
-      </>
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    
+    </>
   );
 }
-
 export default App;
